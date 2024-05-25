46dbbc1c4bff061932fc45390172b23b476160b3
avneetsohi <avneetsohi107@gmail.com>
2024-05-26 08:09:09 +1000

    Revert "Commit Reversed Revert "Dashboard Search Added""
    This reverts commit 913f5909554e0ba0b427ba9e1245ae96f7ba69bd.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,23 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { setToken } from '../slices/authSlice'
+import apiConnector from '../services/apiconnector'
+import { books } from '../services/apis'
+import toast from 'react-hot-toast'
+import { BooksGrid } from '../components/common/BooksGrid'
+import { FILTER_TYPE } from '../utils/constants'
 
 export const Dashboard = () => {
   
   const navigate=useNavigate()
   const dispatch=useDispatch()
 
-  function clickHandler(){
-    localStorage.setItem("token",null)
-    dispatch(setToken(null))
-    navigate("/login")
+  const [booksList,setBooksList]=useState([])
+  const [loading,setLoading]=useState(false)
+  const [searchValue,setSearchValue]=useState('')
+  const [filter,setFilter]=useState('All')
+ 
+  const fetchAllBooks = async(searchVal='',fil='All') => {
+    try{
+      setLoading(true)
+      const response = await apiConnector("POST",books.ALL_BOOKS_API,{fil,searchVal})
+      if(!response.data.success){
+        throw new Error(response.data.message)
+      }
+      setBooksList(response?.data?.data)
+      toast.success("Fetched All Books")
+      setLoading(false)
+    }
+    catch(error){
+      setLoading(false)
+      console.log(error)
+      toast.error("Failed to fetch books")
+    }
+  }
+  
+  const submitHandler = async(event) => {
+    event.preventDefault()
+    fetchAllBooks(searchValue,filter)
+    setSearchValue("")
+  }
+  
+  const changeHandler = (event) => {
+    setSearchValue(event.target.value)
   }
 
-  return (
-    <div className="w-full h-full flex items-center justify-center">
+  useEffect(()=>{
+    fetchAllBooks()
+  },[])
 
-        <div className='bg-yellow-50 py-5 px-3'>Dashboard</div>
+  return (
+    <div className="w-full h-full flex flex-col justify-center">
+        {
+          loading?(<div className='custom-loader mx-auto mt-44'></div>):(
+            <div>
+                  <div className='bg-richblack-800 py-14 flex-col gap-y-4 items-center  w-full'>
+                  <div className='flex flex-row items-center w-[60%] mx-auto'>
+                      <form onSubmit={submitHandler} className='mx-auto mt-9 w-full'>
+                        <div className='flex flex-row items-center justify-center gap-x-2 w-full'>
+                            <input className='outline-none p-3 rounded-[8px] bg-richblack-500 text-richblack-200 text-base font-medium w-[80%]' type='text' placeholder={`Enter ${filter}...`} id='filter' name='filter'
+                            value={searchValue} onChange={changeHandler} required/>  
+                            <button className={`mt-9 text-center  text-base font-medium p-3 rounded-md bg-yellow-50 text-black w-[20%]
+                            hover:scale-95 transition-all duration-200 cursor-pointer`}>Search</button>
+                        </div>
+                      </form>
+                    </div>
+                    <div className='flex gap-[5px] p-1 rounded-[500px] mx-auto w-fit bg-richblack-700'>
+                      <div className={`py-[6px] px-[18px] rounded-full cursor-pointer ${filter===FILTER_TYPE.ALL?"bg-richblack-900 text-richblack-5":"text-richblack-200"}`} onClick={()=>{setFilter(FILTER_TYPE.ALL)}}>All</div>
+                      <div className={`py-[6px] px-[18px] rounded-full cursor-pointer ${filter===FILTER_TYPE.TITLE?"bg-richblack-900 text-richblack-5":"text-richblack-200"}`} onClick={()=>{setFilter(FILTER_TYPE.TITLE)}}>Title</div>
+                      <div className={`py-[6px] px-[18px] rounded-full cursor-pointer ${filter===FILTER_TYPE.AUTHOR?"bg-richblack-900 text-richblack-5":"text-richblack-200"}`} onClick={()=>{setFilter(FILTER_TYPE.AUTHOR)}}>Author</div>
+                      <div className={`py-[6px] px-[18px] rounded-full cursor-pointer ${filter===FILTER_TYPE.YEAR?"bg-richblack-900 text-richblack-5":"text-richblack-200"}`} onClick={()=>{setFilter(FILTER_TYPE.YEAR)}}>Year</div>
+                    </div>     
+              </div>
+
+              <div className='py-10 mx-auto w-[80%]'>
+                <div className=''>
+                    {
+                        booksList?.length===0?(
+                            <p className=' text-[18px] text-richblack-200 font-medium'>No Books Available Right Now.</p>
+                        ):(
+                            <BooksGrid booksList={booksList}/>
+                        )
+                    }
+                </div>
+              </div>
+            </div>
+          )
+        }
     </div>
   )
 }
--- src/pages/Genre.js
+++ src/pages/Genre.js
@@ -1,54 +1,84 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import apiConnector from '../services/apiconnector'
-import { books } from '../services/apis'
+import { books, categories } from '../services/apis'
 import toast from 'react-hot-toast'
 import { useParams } from 'react-router-dom'
 import { BooksGrid } from '../components/common/BooksGrid'
+import { setCategory } from '../slices/categorySlice'
 
 export const Genre = () => {
 
   const {category}=useSelector((state)=>state.category)
-  const [books,setBooks]=useState([])
+  const [bookList,setBookList]=useState([])
   const [loading,setLoading]=useState(false)
   const {genreName}=useParams()
+  const dispatch = useDispatch()
+  
+
   
   const fetchBooks = async() => {
     try{
         setLoading(true)
-        const response=await apiConnector("GET",books.GENRE_BOOKS_API,{categoryID:category._id})
+        const response=await apiConnector("POST",books.GENRE_BOOKS_API,{categoryID:category._id})
 
         if(!response.data.success){
             throw new Error(response.data.message)
         }
-        setBooks(response?.data?.data)
+        setBookList(response?.data?.data)
         toast.success(`Fetched ${category.name} Genre Books Successfully`)
         setLoading(false)
 
     }catch(error){
         setLoading(false)
-        console.log("Error occured while fetching category details page",error)
-        toast.error("Failed to fetch selected category details")
+        console.log("Error occured while fetching Genre details page",error)
+        toast.error("Failed to fetch selected Genre Books")
     }
   }
 
+  const getCategory = async() => {
+    try{
+        const catgName=genreName.split("-")
+        for (let i = 0; i < catgName.length; i++) {
+            catgName[i] = catgName[i][0].toUpperCase() + catgName[i].substr(1);
+        }
+        const response=await apiConnector("PUT",categories.GET_CATEGORY,{name:catgName})
+        if(!response.data.success){
+            throw new Error(response.data.message)
+        }
+        dispatch(setCategory(response?.data?.data._id))
+
+    }catch(error){
+        console.log("Error occured while fetching Genre details page",error)
+        toast.error("Failed to fetch selected Genre Books")
+    }
+  }
 
   useEffect(()=>{
     fetchBooks()
   },[category])
 
+
+  useEffect(()=>{
+    getCategory()
+  },[genreName])
+
+
+
+  
+
   return (
     <div>
         {
             loading?(<div className='custom-loader mx-auto mt-44'></div>):(
-                <div>
+                <div className='mx-auto'>
                     {
                         !category?(
                             <p className='text-[26px] mt-44 text-center text-richblack-200 font-medium'>
                                 Please select a Genre from the Category dropdown menu.
                             </p>
                         ):(
-                            <div>
+                            <div className=''>
                                 <div className='bg-richblack-800 py-14'>
                                     <div className='w-11/12 max-w-maxContent mx-auto flex flex-col gap-5 justify-between'>
                                         <h1 className='capitalize text-[30px] text-richblack-5'>{genreName.replace("-"," ")}</h1>
@@ -56,13 +86,13 @@ export const Genre = () => {
                                     </div>
                                 </div>
 
-                                <div className='py-10'>
-                                        <div className='mt-4 mx-auto flex flex-col items-center '>
+                                <div className='py-10 mx-auto w-[80%]'>
+                                        <div className=''>
                                             {
                                                 books?.length===0?(
                                                     <p className=' text-[18px] text-richblack-200 font-medium'>No Books Available Right Now.</p>
                                                 ):(
-                                                    <BooksGrid booksList={books}/>
+                                                    <BooksGrid booksList={bookList}/>
                                                 )
                                             }
                                        </div>
